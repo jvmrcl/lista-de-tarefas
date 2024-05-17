d75874b57b215ebb8143aa8eee8099f741b78dfb
João Marcon <166364834+jvmrcl@users.noreply.github.com>
2024-05-17 12:56:11 -0300

    Testing a new config for index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,4 @@ import Application from './App';
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Application />
-  </React.StrictMode>
-);
+root.render(<Application />);
